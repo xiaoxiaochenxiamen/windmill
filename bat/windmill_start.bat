3123a46bddfd24717e102a7e15961ae5b691d565
chenjun <18628339855@163.com>
2015-12-18 15:23:38 +0800

    a

--- bat/windmill_start.bat
+++ bat/windmill_start.bat
@@ -1,8 +1,15 @@
 set NODE_NAME=windmill
-set CONFIG=run_windmill
+set CONFIG=windmill
 set SMP=auto
 set ERL_PROCESSES=1024000
 cd ../config
-erl  +P %ERL_PROCESSES% -smp %SMP% -env ERL_MAX_PORTS 65535 -pa ../ebin -sname %NODE_NAME% -config %CONFIG% -boot start_sasl -s  windmill_app start_windmill_app
+erl  +P %ERL_PROCESSES% \
+     -smp %SMP% \
+     -env ERL_MAX_PORTS 65535 \
+     -pa ../ebin ../Emysql/ebin \
+    -sname %NODE_NAME% \
+    -config %CONFIG% \
+    -boot start_sasl \
+    -s  windmill_app start_app
 
 pause
